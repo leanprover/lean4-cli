77ab178b4b9b1bb6aa5612233b842e2898f1a52f
mhuisi <mhuisi@protonmail.com>
2022-02-19 20:22:54 +0100

    chore: move to lake

--- Cli/Tests.lean
+++ Cli/Tests.lean
@@ -1,4 +1,3 @@
-import AssertCmd
 import Cli.Basic
 import Cli.Extensions
 
@@ -87,120 +86,120 @@ def testCmd : Cmd := `[Cli|
 ]
 
 section ValidInputs
-  #assert
+  #eval
     (testCmd.processParsed "foo 1 -ta")
     == "cmd: testcommand; flags: #[--typed1=a, --level-param=0]; positionalArgs: #[<input1=foo>, <input2=1>]; variableArgs: #[]"
 
-  #assert
+  #eval
     (testCmd.processParsed "-h")
     == "cmd: testcommand; flags: #[--help, --level-param=0]; positionalArgs: #[]; variableArgs: #[]"
 
-  #assert
+  #eval
     (testCmd.processParsed "--version")
     == "cmd: testcommand; flags: #[--version, --level-param=0]; positionalArgs: #[]; variableArgs: #[]"
 
-  #assert
+  #eval
     (testCmd.processParsed "foo --verbose -p-n 2 1,2,3 1 -xnx 2 --typed1=foo 3")
     == "cmd: testcommand; flags: #[--verbose, --level-param=2, --unknown2, --unknown1, --typed1=foo]; positionalArgs: #[<input1=foo>, <input2=1,2,3>]; variableArgs: #[<outputs=1>, <outputs=2>, <outputs=3>]"
 
-  #assert
+  #eval
     (testCmd.processParsed "foo -xny 1 -t 3")
     == "cmd: testcommand; flags: #[--unknown1, --unknown3, --typed1=3, --level-param=0]; positionalArgs: #[<input1=foo>, <input2=1>]; variableArgs: #[]"
 
-  #assert
+  #eval
     (testCmd.processParsed "-t 3 -- --input 2")
     == "cmd: testcommand; flags: #[--typed1=3, --level-param=0]; positionalArgs: #[<input1=--input>, <input2=2>]; variableArgs: #[]"
 
-  #assert
+  #eval
     (testCmd.processParsed "-t1 - 2")
     == "cmd: testcommand; flags: #[--typed1=1, --level-param=0]; positionalArgs: #[<input1=->, <input2=2>]; variableArgs: #[]"
 
-  #assert
+  #eval
     (testCmd.processParsed "-ty -t1 foo 1,2")
     == "cmd: testcommand; flags: #[--typed2, --typed1=1, --level-param=0]; positionalArgs: #[<input1=foo>, <input2=1,2>]; variableArgs: #[]"
 
-  #assert
+  #eval
     (testCmd.processParsed "testsubcommand1 -- testsubsubcommand")
     == "cmd: testcommand testsubcommand1; flags: #[]; positionalArgs: #[<city-location=testsubsubcommand>]; variableArgs: #[]"
 
-  #assert
+  #eval
     (testCmd.processParsed "testsubcommand1 --launch-the-nukes x")
     == "cmd: testcommand testsubcommand1; flags: #[--launch-the-nukes]; positionalArgs: #[<city-location=x>]; variableArgs: #[]"
 
-  #assert
+  #eval
     (testCmd.processParsed "testsubcommand1 -- --launch-the-nukes")
     == "cmd: testcommand testsubcommand1; flags: #[]; positionalArgs: #[<city-location=--launch-the-nukes>]; variableArgs: #[]"
 
-  #assert
+  #eval
     (testCmd.processParsed "testsubcommand1 testsubsubcommand")
     == "cmd: testcommand testsubcommand1 testsubsubcommand; flags: #[]; positionalArgs: #[]; variableArgs: #[]"
 
-  #assert (testCmd.processParsed "testsubcommand2 --run asdf,geh")
+  #eval (testCmd.processParsed "testsubcommand2 --run asdf,geh")
     == "cmd: testcommand testsubcommand2; flags: #[--run]; positionalArgs: #[<ominous-input=asdf,geh>]; variableArgs: #[]"
 end ValidInputs
 
 section InvalidInputs
-  #assert
+  #eval
     (testCmd.processParsed "")
     == "Missing positional argument `<input1>.`"
 
-  #assert
+  #eval
     (testCmd.processParsed "foo")
     == "Missing positional argument `<input2>.`"
 
-  #assert
+  #eval
     (testCmd.processParsed "foo asdf")
     == "Invalid type of argument `asdf` for positional argument `<input2 : Array Nat>`."
 
-  #assert
+  #eval
     (testCmd.processParsed "foo 1,2,3")
     == "Missing required flag `--typed1`."
 
-  #assert
+  #eval
     (testCmd.processParsed "foo 1,2,3 -t")
     == "Missing argument for flag `-t`."
 
-  #assert
+  #eval
     (testCmd.processParsed "foo 1,2,3 -t1 --level-param=")
     == "Invalid type of argument `` for flag `--level-param : Nat`."
 
-  #assert
+  #eval
     (testCmd.processParsed "foo 1,2,3 -t1 -p-n=")
     == "Invalid type of argument `` for flag `-p-n : Nat`."
 
-  #assert
+  #eval
     (testCmd.processParsed "foo 1,2,3 -t1 --asdf")
     == "Unknown flag `--asdf`."
 
-  #assert
+  #eval
     (testCmd.processParsed "foo 1,2,3 -t1 -t2")
     == "Duplicate flag `-t` (`--typed1`)."
 
-  #assert
+  #eval
     (testCmd.processParsed "foo 1,2,3 -t1 --typed1=2")
     == "Duplicate flag `--typed1` (`-t`)."
 
-  #assert
+  #eval
     (testCmd.processParsed "foo 1,2,3 --typed12")
     == "Unknown flag `--typed12`."
 
-  #assert
+  #eval
     (testCmd.processParsed "foo 1,2,3 -t1 -x=1")
     == "Redundant argument `1` for flag `-x` that takes no arguments."
 
-  #assert
+  #eval
     (testCmd.processParsed "foo 1,2,3 -t1 bar")
     == "Invalid type of argument `bar` for variable argument `<outputs : Nat>...`."
 
-  #assert
+  #eval
     (testCmd.processParsed "foo 1,2,3 -t1 -xxn=1")
     == "Unknown flag `-xxn`."
 
-  #assert
+  #eval
     (testCmd.processParsed "foo 1,2,3 --t=1")
     == "Unknown flag `--t`."
 
-  #assert
+  #eval
     (testCmd.processParsed "testsubcommand1 asdf geh")
     == "Redundant positional argument `geh`."
 end InvalidInputs
@@ -238,11 +237,11 @@ section Info
       testsubcommand1  a properly short description
       testsubcommand2  does not do anything interesting
   -/
-  #assert
+  #eval
     testCmd.help
     == "testcommand [0.0.0]\nsome short description that happens to be much longer than necessary and hence\nneeds to be wrapped to fit into an 80 character width limit\n\nUSAGE:\n    testcommand [SUBCOMMAND] [FLAGS] <input1> <input2> <outputs>...\n\nFLAGS:\n    -h, --help                 Prints this message.\n    --version                  Prints the version.\n    --verbose                  a very verbose flag description that also needs\n                               to be wrapped to fit into an 80 character width\n                               limit\n    -x, --unknown1             this flag has a short name\n    -xn, --unknown2            short names do not need to be prefix-free\n    -ny, --unknown3            -xny will parse as -x -ny and not fail to parse\n                               as -xn -y\n    -t, --typed1 : String      flags can have typed parameters\n    -ty, --typed2              -ty parsed as --typed2, not -t=y\n    -p-n, --level-param : Nat  hyphens work, too\n\nARGS:\n    input1 : String     another very verbose description that also needs to be\n                        wrapped to fit into an 80 character width limit\n    input2 : Array Nat  arrays!\n    outputs : Nat       varargs!\n\nSUBCOMMANDS:\n    testsubcommand1  a properly short description\n    testsubcommand2  does not do anything interesting"
 
-  #assert
+  #eval
     testCmd.version
     == "0.0.0"
 
@@ -252,7 +251,7 @@ section Info
   might be.
   Run `testcommand -h` for further information.
   -/
-  #assert
+  #eval
     (testCmd.error "some exceedingly long error that needs to be wrapped to fit within an 80 character width limit. none of our errors are really that long, but flag names might be.")
     == "some exceedingly long error that needs to be wrapped to fit within an 80\ncharacter width limit. none of our errors are really that long, but flag names\nmight be.\nRun `testcommand -h` for further information."
 
@@ -271,7 +270,7 @@ section Info
   ARGS:
       ominous-input : Array String  what could this be for?
   -/
-  #assert
+  #eval
     testSubCmd2.help
     == "testsubcommand2 [0.0.-1]\ndoes not do anything interesting\n\nUSAGE:\n    testsubcommand2 [FLAGS] <ominous-input>\n\nFLAGS:\n    -h, --help  Prints this message.\n    --version   Prints the version.\n    -r, --run   really, this does not do anything. trust me.\n\nARGS:\n    ominous-input : Array String  what could this be for?"
 
@@ -290,7 +289,7 @@ section Info
   ARGS:
       ominous-input : Array String  what could this be for?
   -/
-  #assert
+  #eval
     (testCmd.subCmd! "testsubcommand2").help
     == "testcommand testsubcommand2 [0.0.-1]\ndoes not do anything interesting\n\nUSAGE:\n    testcommand testsubcommand2 [FLAGS] <ominous-input>\n\nFLAGS:\n    -h, --help  Prints this message.\n    --version   Prints the version.\n    -r, --run   really, this does not do anything. trust me.\n\nARGS:\n    ominous-input : Array String  what could this be for?"
 
@@ -305,7 +304,7 @@ section Info
       -h, --help  Prints this message.
       --version   Prints the version.
   -/
-  #assert
+  #eval
     (testCmd.subCmd! "testsubcommand1" |>.subCmd! "testsubsubcommand").help
     == "testcommand testsubcommand1 testsubsubcommand [0.0.2]\ndoes this even do anything?\n\nUSAGE:\n    testcommand testsubcommand1 testsubsubcommand [FLAGS]\n\nFLAGS:\n    -h, --help  Prints this message.\n    --version   Prints the version."
 
@@ -345,7 +344,7 @@ section Info
   DESCRIPTION:
       this could be really long, but i'm too lazy to type it out.
   -/
-  #assert
+  #eval
     (testCmd.update' (meta := testCmd.extension!.extendMeta testCmd.meta)).help
     == "testcommand [0.0.0]\nmhuisi\nsome short description that happens to be much longer than necessary and hence\nneeds to be wrapped to fit into an 80 character width limit\n\nUSAGE:\n    testcommand [SUBCOMMAND] [FLAGS] <input1> <input2> <outputs>...\n\nFLAGS:\n    -h, --help                 Prints this message.\n    --version                  Prints the version.\n    --verbose                  a very verbose flag description that also needs\n                               to be wrapped to fit into an 80 character width\n                               limit\n    -x, --unknown1             this flag has a short name\n    -xn, --unknown2            short names do not need to be prefix-free\n    -ny, --unknown3            -xny will parse as -x -ny and not fail to parse\n                               as -xn -y\n    -t, --typed1 : String      [Required] flags can have typed parameters\n    -ty, --typed2              -ty parsed as --typed2, not -t=y\n    -p-n, --level-param : Nat  hyphens work, too [Default: `0`]\n\nARGS:\n    input1 : String     another very verbose description that also needs to be\n                        wrapped to fit into an 80 character width limit\n    input2 : Array Nat  arrays!\n    outputs : Nat       varargs!\n\nSUBCOMMANDS:\n    testsubcommand1  a properly short description\n    testsubcommand2  does not do anything interesting\n\nDESCRIPTION:\n    this could be really long, but i'm too lazy to type it out."
 end Info
