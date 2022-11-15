cd523a1951a8ec1ffb276446280ac60a7c5ad333
Arthur Paulino <arthurleonardo.ap@gmail.com>
2022-11-15 16:38:49 -0300

    bump toolchain

--- lakefile.lean
+++ lakefile.lean
@@ -7,4 +7,4 @@ package Cli
 lean_lib Cli
 
 require std from git
-  "https://github.com/leanprover/std4"@"f648e43ef696ce1cf7f6ec534ec44c06816380f9"
+  "https://github.com/leanprover/std4/" @ "d83e97c7843deb1cf4a6b2a2c72aaf2ece0b4ce8"
