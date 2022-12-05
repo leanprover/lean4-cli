a452367fa4fdbdf783b9ab4ad0f4f1e8878a72ed
mpenciak <matej.penciak@gmail.com>
2022-12-05 14:51:58 -0500

    fix CLI with toolchain bump

--- lakefile.lean
+++ lakefile.lean
@@ -7,4 +7,4 @@ package Cli
 lean_lib Cli
 
 require std from git
-  "https://github.com/leanprover/std4/" @ "d83e97c7843deb1cf4a6b2a2c72aaf2ece0b4ce8"
+  "https://github.com/leanprover/std4/" @ "cad19f171ad4aebe091e6deeb9a43a14521bfbfe"
