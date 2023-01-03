5a8f4a22248d854db79b78ad2a55976005073ed3
Arthur Paulino <arthurleonardo.ap@gmail.com>
2023-01-03 10:46:48 -0300

    bump YatimaStdLib

--- lakefile.lean
+++ lakefile.lean
@@ -10,4 +10,4 @@ require std from git
   "https://github.com/leanprover/std4/" @ "2919713bde15d55e3ea3625a03546531283bcb54"
 
 require YatimaStdLib from git
-  "https://github.com/yatima-inc/YatimaStdLib.lean" @ "818538aced05fe563ef95bb3dcdf5ed755896139"
+  "https://github.com/yatima-inc/YatimaStdLib.lean" @ "515b7eff7765dcf55ce275ac41fa13a30a59f1d0"
